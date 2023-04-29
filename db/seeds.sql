ee418e5c90ea4f3430aff37cded162c4a0e9ce4f
MisterBham <brianjhamlin@gmail.com>
2023-04-29 14:10:37 -0500

    adding seeds data

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (id, name)
+VALUES  (001, "Leadership"),
+        (002, "Engineering"),
+        (003, "Sales"),
+        (004, "Finance"),
+        (005, "Human Resources");
+       
+INSERT INTO role (title, salary)
+VALUES ("Chief Executive Officer", 212000),
+        ("Chief Engineer", 198000),
+        ("Sales Manager", 135000),
+        ("Chief Financial Officer", 135000),
+        ("Vice President of Human Resources", 100000),
+        ("Sales Lead", 75000),
+        ("Sales Specialist", 65000),
+        ("Engineering Lead", 125000),
+        ("Engineer", 100000),
+        ("Finance Lead", 75000),
+        ("Financial Analyst", 65000),
+        ("HR Lead", 75000),
+        ("Human Resource Analyst", 65000);
+       
+INSERT INTO employee (first_name, last_name, )
