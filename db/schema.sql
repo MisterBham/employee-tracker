28878f2ade18a8dd08947f12c1cf1eac5723f179
Brian Hamlin <brianjhamlin@gmail.com>
2023-04-30 22:04:23 -0500

    reverting to mysql2, modularizing

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,24 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCHAR(30),
+    name VARCHAR(30)
 );
 
--- CREATE TABLE role (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     title VARCHAR(30),
---     salary DECIMAL(8,2),
---     -- department_id INT,
---     -- FOREIGN KEY (department_id)
---     -- REFERENCES department(id)
--- );
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(8,2),
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
 
--- CREATE TABLE employee (
---     id INT PRIMARY KEY AUTO_INCREMENT,
---     first_name VARCHAR(30),
---     last_name VARCHAR(30),
---     -- role_id INT, 
---     manager_id INT,
---     -- FOREIGN KEY (role_id)
---     -- REFERENCES role(id)
--- )
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
--- index.js
+++ index.js
@@ -1,86 +1,72 @@
-const { stringify } = require('querystring');
-const sequelize = require('./config/connection');
-const inquirer = require('inquirer');
-// const Department = require('./models/department');
-// const Employee = require('./models/employee');
-// const Role = require('./models/role');
+
+const ASCII = require("./lib/ASCII");
+
+//  SYNC WITH DB
+const db = require('./config/connection');
+
+db.connect(function (err) {
+    if (err) throw err;
+})
+
+
+// const init = () => {
+//     inquirer
+//         .prompt(task)
+//         .then((answer) => {
+//             console.log(answer);
+//             switch (answer.Selection) {
+//                 case answer.Selection = "View all employees":
+//                     console.log(answer.Selection);
+//                     viewAllEmployees();
+//                     break;
+//                 case answer.Selection = "Add employee":
+//                     console.log(answer.Selection);
+//                     addEmployee();
+//                     break;
+//                 case answer.Selection = "Update employee role":
+//                     console.log(answer.Selection);
+//                     updateEmployeeRole();
+//                     break;
+//                 case answer.Selection = "Add role":
+//                     console.log(answer.Selection);
+//                     addRole();
+//                     break;
+//                 case answer.Selection = "View all departments":
+//                     console.log(answer.Selection);
+//                     viewAllDepartments();
+//                     break;
+//                 case answer.Selection = "Add department":
+//                     console.log(answer.Selection);
+//                     addDepartment();
+//                     break;
+//                 case answer.Selection = "Quit":
+//                     console.log(answer.Selection);
+//                     process.exit(0);
+//             }
+//         });
+// };
 
 // SETUP DIFFERENT ACTIONS
 // let viewAllEmployees = () => {
-//     console.log(`not working`);
-//     // let allEmployees = Employee.findAll()
-//     //     .then((data) => {
-//     //         console.table(data);
-//     //         init();
-//     //     })
+//     return console.log(`not working`);
 // };
 
 // let addEmployee = () => {
-//     res.send(`asajdsfasdf`);
+//     return console.log(`not working`);
 // };
 // let updateEmployeeRole = () => {
-//     res.send(`asajdsfasdf`);
+//     return console.log(`not working`);
 // };
 // let addRole = () => {
-//     res.send(`asajdsfasdf`);
+//     return console.log(`not working`);
 // };
 // let viewAllDepartments = () => {
-//     res.send(`asajdsfasdf`);
+//     return console.log(`not working`);
 // };
 // let addDepartment = () => {
-//     res.send(`asajdsfasdf`);
+//     return console.log(`not working`);
 // };
 // let quit = () => {
-//     process.exit(0);
+//     return console.log(`not working`);
+//     // process.exit(0);
 // };
-
-//  SYNC WITH DB 
-sequelize.sync({ force: true }).then(() => {
-    console.log(`Synced with SQL`);
-    init();
-});
-
-const init = () => {
-    console.info(`ASCII ART HERE`);
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: "Selection",
-                message: "What would you like to do?",
-                choices: ["View all employees", "Add employee", "Update employee role", "View all roles", "Add role", "View all departments", "Add department", "Quit"],
-            }
-        ])
-        .then((selection) => {
-            console.log(JSON.stringify(selection).substr(13, 35));
-            switch (selection) {
-                case "View all employees":
-                    return console.log(`Viewing all employees`);
-                // viewAllEmployees();
-                case "Add employee":
-                    return console.log(`Viewing all employees`);
-                // addEmployee();
-                case "Update employee role":
-                    return console.log(`Viewing all employees`);
-                // updateEmployeeRole();
-                case "Add role":
-                    return console.log(`Viewing all employees`);
-                // addRole();
-                case "View all departments":
-                    return console.log(`Viewing all employees`);
-                // viewAllDepartments();
-                case "Add department":
-                    return console.log(`Viewing all employees`);
-                // addDepartment();
-                case "Quit":
-                    return console.log(`Viewing all employees`);
-                // quit();
-            }
-        }, (err) => {
-            console.error(err);
-        })
-        .catch((err) => {
-            if (err) throw err;
-        });
-
-};
